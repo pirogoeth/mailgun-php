04a69e54134492d04bac115d8088f8ced0aaac20
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-10-14 22:20:06 +0200

    Minor

--- src/Mailgun/Assert.php
+++ src/Mailgun/Assert.php
@@ -5,6 +5,8 @@ namespace Mailgun;
 use Mailgun\Exception\InvalidArgumentException;
 
 /**
+ * We need to override Webmozart\Assert because we want to throw our own Exception.
+ *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
 class Assert extends \Webmozart\Assert\Assert
